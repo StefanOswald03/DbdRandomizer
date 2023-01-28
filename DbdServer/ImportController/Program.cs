3e4b81ee677c1b9114a8a144ef619a15abb7a02d
StefanOswald03 <55189398+StefanOswald03@users.noreply.github.com>
2023-01-28 23:30:48 +0100

    Create ImportConsole App

---
+++ DbdServer/ImportController/Program.cs
@@ -0,0 +1,7 @@
+﻿using Persistence;
+
+Console.WriteLine("Import der Movies und Categories in die Datenbank");
+await using var unitOfWork = new UnitOfWork();
+Console.WriteLine("Clear Tables Perk and Category");
+await unitOfWork.Perk.ClearTable();
+await unitOfWork.Category.ClearTable();
