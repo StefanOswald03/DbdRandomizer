982f76f8d649e9fc13c6ed2ac5a6de5b0c7a79ae
StefanOswald03 <55189398+StefanOswald03@users.noreply.github.com>
2023-01-29 14:54:57 +0100

    work on json perk Import

--- DbdServer/ImportController/Program.cs
+++ DbdServer/ImportController/Program.cs
@@ -1,7 +1,36 @@
-﻿using Persistence;
+﻿using System.Drawing.Text;
+using Base.Helper;
+using Core.Entities;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Persistence;
+
+const string PerkFileName = "perks.json";
 
 Console.WriteLine("Import der Movies und Categories in die Datenbank");
-await using var unitOfWork = new UnitOfWork();
+//await using var unitOfWork = new UnitOfWork();
 Console.WriteLine("Clear Tables Perk and Category");
-await unitOfWork.Perk.ClearTable();
-await unitOfWork.Category.ClearTable();
+//await unitOfWork.Perk.ClearTable();
+//await unitOfWork.Category.ClearTable();
+await GetPerksFromJsonAsync();
+
+async Task GetPerksFromJsonAsync()
+{
+    var path = MyFile.GetFullNameInApplicationTree(PerkFileName);
+    if (path.IsNullOrEmpty())
+        return;
+
+    var js = new JsonSerializer();
+
+    var json = await File.ReadAllTextAsync(path!);
+    dynamic jsonObject = JsonConvert.DeserializeObject(json);
+    List<Perk> perks = new List<Perk>();
+    foreach (var perk in jsonObject)
+    {
+        Perk p = new Perk();
+        p.Name = perk.Value.name;
+        p.Description = perk.Value.description;
+        p.Role = perk.Value.role;
+        perks.Add(p);
+    }
+}
